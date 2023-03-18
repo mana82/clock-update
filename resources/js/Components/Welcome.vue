e2ae14ed0c020f290a51e9f4d60995a440bfbd41
manojef <XASgm4c_yrzC@x2>
2023-03-18 15:17:30 +0200

    Add Jetstream pakcage

---
+++ resources/js/Components/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+
+            <h1 class="text-2xl font-medium text-gray-900">
+                Welcome to Clock Update
+            </h1>
+
+            
+        </div>
+
+        <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+            
+            <div>
+                <div class="flex items-center">
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
